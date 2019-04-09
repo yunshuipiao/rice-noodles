728414af9e7cd3262a62aa9f1f64fad5af1727dc
sunwen <1416316137@qq.com>
2019-04-10 01:00:34 +0800

    update readme 2 -- 10

--- kotlin/src/Main.kt
+++ kotlin/src/Main.kt
@@ -1,5 +1,22 @@
+import java.io.File
+
 fun main(args: Array<String>) {
     println("leetcode of kotlin ")
+   createReadme(2, 10)
+
+}
+
+fun createReadme(from: Int, to: Int) {
+    (from..to).forEach {
+        val folderPath = String.format("_%04d", it)
+        val ktFile = File("kotlin/src/$folderPath/$folderPath.kt")
+        val readMe = File("kotlin/src/$folderPath/Readme.md")
+        ktFile.parentFile.mkdirs()
+        ktFile.createNewFile()
+        readMe.createNewFile()
+//        File("$folderPath/Readme.md").createNewFile()
+//        File("kotlin/src/_$folderPath/Readme.md").createNewFile()
+    }
 }
 
 
